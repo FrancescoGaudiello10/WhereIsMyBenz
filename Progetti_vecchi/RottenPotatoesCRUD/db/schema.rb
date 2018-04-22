6937c032bcc0dddbdeec6a24d390876d313779b8
marco2012 <marco.fox.marco@gmail.com>
2018-04-22 11:05:23 +0200

    aggiornato progetto

--- Progetti_vecchi/RottenPotatoesCRUD/db/schema.rb
+++ Progetti_vecchi/RottenPotatoesCRUD/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180417082347) do
+ActiveRecord::Schema.define(version: 20180405092544) do
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
@@ -19,9 +19,6 @@ ActiveRecord::Schema.define(version: 20180417082347) do
     t.datetime "release_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "reviews", force: :cascade do |t|
   end
 
   create_table "users", force: :cascade do |t|
