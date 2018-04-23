b6fb332a19f87a6794118690a0210417610ea3d7
marco2012 <marco.fox.marco@gmail.com>
2018-04-23 21:50:05 +0200

    homepage

--- carburante2/app/assets/javascripts/application.js
+++ carburante2/app/assets/javascripts/application.js
@@ -11,6 +11,9 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require activestorage
 //= require turbolinks
+//= require bootstrap
 //= require_tree .
+//= require jquery 
+//= require jquery_ujs
+
--- carburante2/config/routes.rb
+++ carburante2/config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
 
+  get 'welcome/index'
 
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
   match ':controller(/:action(/:id))', :via => :get
-  match '/signup',  :to => 'users#new', :via => :get
+  
+  root 'welcome#index'
+
 end
