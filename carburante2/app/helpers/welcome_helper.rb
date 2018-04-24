a349594c9fb744c74d0d48f30d58beeed4cb7261
marco2012 <marco.fox.marco@gmail.com>
2018-04-24 22:45:18 +0200

    ricerca test

--- carburante2/app/helpers/welcome_helper.rb
+++ carburante2/app/helpers/welcome_helper.rb
@@ -1,2 +1,15 @@
 module WelcomeHelper
+
+    def tipo_carburante
+        [
+            ['Benzina', 'Benzina'],
+            ['Blue Super', 'Blue Super'],
+            ['Gasolio','Gasolio'],
+            ['Blue Diesel','Blue Diesel'],
+            ['Excellium Diesel','Excellium Diesel'],
+            ['Metano','Metano'],
+            ['GPL','GPL']
+        ]
+    end
+
 end
