1b20bde58f3e8ce81cacff719ca3998ec4cd2c0d
marco2012 <marco.fox.marco@gmail.com>
2018-04-25 22:49:56 +0200

    db

---
+++ carburante2/app/controllers/implants_controller.rb
@@ -0,0 +1,38 @@
+class ImplantsController < ApplicationController
+
+    def index
+        @city = params[:city].upcase
+        @tipo_carburante = params[:tipo_carburante]
+        @litri_rimanenti = params[:litri_rimanenti]
+        # https://stackoverflow.com/a/34311227/1440037
+        @implant = Implant
+                       .select('Implants.*, prices.*')
+                       .joins('INNER JOIN prices ON Implants.idImpianto = prices.idImpianto')
+                       .where('Comune = ? AND descCarburante = ?', @city, @tipo_carburante)
+                       .order('prices.prezzo asc')
+                       .limit(20)
+    end
+
+    def show
+        @id = params[:id]
+        @implant = Implant
+                       .select('Implants.*, prices.*')
+                       .joins('INNER JOIN prices ON Implants.idImpianto = prices.idImpianto')
+                       .where('Implants.idImpianto = ?', @id)
+                       .group('prices.descCarburante')
+        #https://apidock.com/rails/ActiveRecord/Calculations/pluck
+        @Bandiera   = @implant.pluck(:Bandiera)[0]
+        @Indirizzo  = @implant.pluck(:Indirizzo)[0]
+        @Comune     = @implant.pluck(:Comune)[0]
+        @Provincia  = @implant.pluck(:Provincia)[0]
+        @carburanti = @implant.pluck(:descCarburante)
+        @lat        = @implant.pluck(:Latitudine)[0]
+        @long       = @implant.pluck(:Longitudine)[0]
+    end
+
+    private
+    def implant_params
+        params.require(:implant).permit(:Gestore, :Bandiera, :TipoImpianto, :NomeImpianto, :Indirizzo, :Comune, :Provincia, :Latitudine, :Longitudine)
+    end
+
+end
