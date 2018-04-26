ab9670273cc96ea3d71fa6d5a7dc5777f39dd941
marco2012 <marco.fox.marco@gmail.com>
2018-04-26 14:57:57 +0200

    meteo

--- carburante2/app/controllers/implants_controller.rb
+++ carburante2/app/controllers/implants_controller.rb
@@ -22,7 +22,6 @@ class ImplantsController < ApplicationController
         @implant.save #salva nel database, valore booleano
     end
 
-    require 'httparty'
     def show
         @id = params[:id]
         @implant = Implant
@@ -40,12 +39,32 @@ class ImplantsController < ApplicationController
         @lat        = @implant.pluck(:Latitudine)[0]
         @long       = @implant.pluck(:Longitudine)[0]
 
-        @weather = HTTParty.get('http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=cdcc43f188d9a63e00471c9b6b45cada').parsed_response
+        get_weather
+
     end
 
     private
     def implant_params
         params.require(:implant).permit(:idImpianto, :Gestore, :Bandiera, :TipoImpianto, :NomeImpianto, :Indirizzo, :Comune, :Provincia, :Latitudine, :Longitudine)
+    end
+
+    def get_weather
+        @weather = HTTParty.get(
+            "http://api.openweathermap.org/data/2.5/weather?q=#{@Comune}&appid=cdcc43f188d9a63e00471c9b6b45cada&lang=it&units=metric",
+            :query => {:output => 'json'}
+        )
+        @weather_description = @weather["weather"][0]["description"]
+        @weather_temp = @weather["main"]["temp"]
+        @weather_temp_min = @weather["main"]["temp_min"]
+        @weather_temp_max = @weather["main"]["temp_max"]
+        @weather_sunrise = unixToHuman(@weather["sys"]["sunrise"])
+        @weather_sunset = unixToHuman(@weather["sys"]["sunset"])
+    end
+
+    #converte timestamp unix in orario normale
+    def unixToHuman (timestamp)
+        #https://stackoverflow.com/a/3964560/1440037
+        Time.at(timestamp).utc.in_time_zone(+2).strftime("%H:%M:%S")
     end
 
 end
--- carburante2/app/models/implant.rb
+++ carburante2/app/models/implant.rb
@@ -1,3 +1,4 @@
 class Implant < ApplicationRecord
+    require 'httparty'
     has_many :prices
 end
