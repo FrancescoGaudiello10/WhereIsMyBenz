263b5ebb8fab58cd44b0a83de43c15f4f3c90327
marco2012 <marco.fox.marco@gmail.com>
2018-04-28 16:59:08 +0200

    fix stazioni visitate

--- carburante2/app/controllers/calcs_controller.rb
+++ carburante2/app/controllers/calcs_controller.rb
@@ -22,11 +22,16 @@ class CalcsController < ApplicationController
     private
 
     def salvaImpiantoDB(id)
-        if id.is_a? Numeric
+        if is_number(id)
             @visitati = current_user.idImpianto #impianti attualmente in DB
             @visitati.append(id) unless @visitati.include?(id) #aggiungo l'impianto cercato
             current_user.update_attribute :idImpianto, @visitati  #scrivo nel DB
         end
+    end
+
+    # https://stackoverflow.com/a/26062228/1440037
+    def is_number(n)
+        !!(n =~ /\A[-+]?[0-9]+(\.[0-9]+)?\z/)
     end
 
 end
