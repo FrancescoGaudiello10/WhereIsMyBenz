867890b197752b59af1630015ab73f6a778b470f
marco2012 <marco.fox.marco@gmail.com>
2018-05-03 19:27:32 +0200

    fix grafico

--- carburante2/app/controllers/comments_controller.rb
+++ carburante2/app/controllers/comments_controller.rb
@@ -2,20 +2,20 @@ class CommentsController < ApplicationController
 
     def create
         @station = Station.find(params[:station_id])
-        @review = @station.reviews.create(comment_params)
+        @comment = @station.comments.create(comment_params)
         redirect_to station_path(@station)
     end
 
     def destroy
         @station = Station.find(params[:station_id])
-        @review = @station.reviews.find(params[:id])
+        @comment = @station.comments.find(params[:id])
         @comment.destroy
         redirect_to station_path(@station)
     end
 
     private
     def comment_params
-        params.require(:review).permit(:commenter, :body, :stars)
+        params.require(:comment).permit(:commenter, :body, :stars)
     end
 
 end
