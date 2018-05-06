c3177790cf4295920f0cbc67887edf3f1c6bf1fb
marco2012 <marco.fox.marco@gmail.com>
2018-05-06 13:05:02 +0200

    dettagli automobile da targa

--- carburante2/db/migrate/20180429082459_add_profile_to_users.rb
+++ carburante2/db/migrate/20180429082459_add_profile_to_users.rb
@@ -4,5 +4,6 @@ class AddProfileToUsers < ActiveRecord::Migration[5.2]
     add_column :users, :stazione_preferita, :string
     add_column :users, :carburante_preferito, :string
     add_column :users, :litri_serbatoio, :integer
+    add_column :users, :targa, :string
   end
 end
