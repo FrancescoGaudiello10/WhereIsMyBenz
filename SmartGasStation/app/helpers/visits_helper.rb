753b5648e0c31eddbaa7dffabbf39110d3900702
marco2012 <marco.fox.marco@gmail.com>
2018-07-25 18:42:49 +0200

    fix logo stazione non trovato e diagramma er

--- SmartGasStation/app/helpers/visits_helper.rb
+++ SmartGasStation/app/helpers/visits_helper.rb
@@ -1,9 +1,14 @@
 module VisitsHelper
     def get_implant_logo(query)
-        res = HTTParty.get(
+    	begin  
+		    res = HTTParty.get(
             "https://api.qwant.com/api/search/images?q=#{query}%20logo",
             :query => {:output => 'json'}
-        )
-        return res["data"]["result"]["items"][0]["media"]
+        	)
+        	return res["data"]["result"]["items"][0]["media"]
+		rescue 
+		    return ""
+		end 
     end
 end
+
--- SmartGasStation/spec/factories.rb
+++ SmartGasStation/spec/factories.rb
@@ -7,13 +7,13 @@ FactoryBot.define do
     station nil
   end
 
-    factory :user do
-        id "1"
-        email "lollo@gmail.com"
-        password "123456"
-        password_confirmation "123456"
-        admin "1"
-    end
+factory :user do
+    id "1"
+    email "lollo@gmail.com"
+    password "123456"
+    password_confirmation "123456"
+    admin "1"
+end
 
 end
 
